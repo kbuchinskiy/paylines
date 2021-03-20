88cc0e0b89cb454585cbd029f74ffb53747cf782
Kyrylo Buchynskyi <kyrylo.buchynskyi@onseo.biz>
2021-03-20 11:09:17 +0200

    format

--- paylines.js
+++ paylines.js
@@ -1,32 +1,30 @@
-
-const configuration =
-  [
-    [1, 1, 1, 1, 1, 1],
-    [2, 0, 0, 0, 0, 0],
-    [3, 2, 2, 2, 2, 2],
-    [4, 0, 1, 2, 1, 0],
-    [5, 2, 1, 0, 1, 2],
-    [6, 1, 0, 1, 0, 1],
-    [7, 1, 2, 1, 2, 1],
-    [8, 0, 1, 0, 1, 0],
-    [9, 2, 1, 2, 1, 2],
-    [10, 1, 0, 0, 0, 1],
-    [11, 1, 2, 2, 2, 1],
-    [12, 2, 2, 1, 2, 2],
-    [13, 0, 0, 1, 0, 0],
-    [14, 2, 1, 1, 1, 2],
-    [15, 0, 1, 1, 1, 0],
-    [16, 0, 2, 0, 2, 0],
-    [17, 2, 0, 2, 0, 2],
-    [18, 1, 1, 0, 1, 1],
-    [19, 1, 1, 2, 1, 1],
-    [20, 2, 2, 0, 2, 2],
-    [21, 0, 0, 2, 0, 0],
-    [22, 0, 0, 1, 2, 2],
-    [23, 2, 2, 1, 0, 0],
-    [24, 1, 0, 2, 0, 1],
-    [25, 1, 2, 0, 2, 1]
-  ];
+const configuration = [
+  [1, 1, 1, 1, 1, 1],
+  [2, 0, 0, 0, 0, 0],
+  [3, 2, 2, 2, 2, 2],
+  [4, 0, 1, 2, 1, 0],
+  [5, 2, 1, 0, 1, 2],
+  [6, 1, 0, 1, 0, 1],
+  [7, 1, 2, 1, 2, 1],
+  [8, 0, 1, 0, 1, 0],
+  [9, 2, 1, 2, 1, 2],
+  [10, 1, 0, 0, 0, 1],
+  [11, 1, 2, 2, 2, 1],
+  [12, 2, 2, 1, 2, 2],
+  [13, 0, 0, 1, 0, 0],
+  [14, 2, 1, 1, 1, 2],
+  [15, 0, 1, 1, 1, 0],
+  [16, 0, 2, 0, 2, 0],
+  [17, 2, 0, 2, 0, 2],
+  [18, 1, 1, 0, 1, 1],
+  [19, 1, 1, 2, 1, 1],
+  [20, 2, 2, 0, 2, 2],
+  [21, 0, 0, 2, 0, 0],
+  [22, 0, 0, 1, 2, 2],
+  [23, 2, 2, 1, 0, 0],
+  [24, 1, 0, 2, 0, 1],
+  [25, 1, 2, 0, 2, 1],
+];
 
 function Paylines(params = { parentElementSelector, configuration }) {
   this.parentElement = document.querySelector(params.parentElementSelector);
@@ -37,9 +35,7 @@ function Paylines(params = { parentElementSelector, configuration }) {
   this.createPaylines();
 }
 
-
 Paylines.prototype = {
-
   defineRowsAmount() {
     this.rowsAmount = this.lines.reduce((a, x) => {
       const n = Math.max(...x.slice(1));
@@ -67,18 +63,20 @@ Paylines.prototype = {
 
   createReel(paylineData, reelNumber) {
     const reel = document.createElement("DIV");
-    reel.classList.add("reel")
+    reel.classList.add("reel");
 
     for (let j = 0; j < this.rowsAmount; j++) {
       const icon = document.createElement("DIV");
       icon.classList.add("icon");
-      j === paylineData.slice(1)[reelNumber] && icon.classList.add("win")
+      j === paylineData.slice(1)[reelNumber] && icon.classList.add("win");
       reel.appendChild(icon);
     }
 
     return reel;
-  }
-  
-}
+  },
+};
 
-const paylines = new Paylines({ parentElementSelector: ".paylines-wrapper", configuration });
+const paylines = new Paylines({
+  parentElementSelector: ".paylines-wrapper",
+  configuration,
+});
